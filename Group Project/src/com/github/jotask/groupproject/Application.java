dc48d3d5165e18243134bb5bd32a61c0008cd881
Jose Vives <jota_sk@hotmail.com>
2015-10-20 20:41:01 +0100

    Created the main class

---
+++ Group Project/src/com/github/jotask/groupproject/Application.java
@@ -0,0 +1,25 @@
+package com.github.jotask.groupproject;
+
+/**
+ * Start class where all start
+ * @author Jose Vives
+ *
+ */
+public class Application {
+	
+	/**
+	 * Constructor for initialize variables
+	 */
+	public Application() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * This method is the first methods called when the program start
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Application();
+	}
+	
+}
